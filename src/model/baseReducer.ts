a9cd4fdd3b5f723f5b3b2d0170e14a5b14e97986
Ted Shaffer <ted@brightsign.biz>
2021-05-25 06:51:47 -0700

    baseReducer, but with some typescript and/or redux issues.

--- src/model/baseReducer.ts
+++ src/model/baseReducer.ts
@@ -1,3 +1,66 @@
+// /** @module Model:base */
+
+// import {
+//   Reducer,
+//   combineReducers
+// } from 'redux';
+// import { isNil } from 'lodash';
+// import { BsUiModelState } from '../type';
+// import {
+//   BSUIMODEL_BATCH,
+//   BsUiModelBaseAction,
+//   BsUiModelBatchAction,
+// } from './baseAction';
+// import {
+//   templateReducer,
+//   isValidTemplateState,
+// } from './template';
+
+// // -----------------------------------------------------------------------
+// // Defaults
+// // -----------------------------------------------------------------------
+
+// // none
+
+// // -----------------------------------------------------------------------
+// // Reducers
+// // -----------------------------------------------------------------------
+
+// export type BsUiReducer = Reducer<BsUiModelState>;
+// const enableBatching = (
+//     reduce: (state: BsUiModelState, action: BsUiModelBaseAction | BsUiModelBatchAction) => BsUiModelState,
+// ): BsUiReducer => {
+//   return function batchingReducer(
+//     state: BsUiModelState,
+//     action: BsUiModelBaseAction | BsUiModelBatchAction,
+//   ): BsUiModelState {
+//     switch (action.type) {
+//       case BSUIMODEL_BATCH:
+//         return (action as BsUiModelBatchAction).payload.reduce(batchingReducer, state);
+//       default:
+//         return reduce(state, action);
+//     }
+//   };
+// };
+
+// export const bsUiModelReducer: BsUiReducer = enableBatching(combineReducers<BsUiModelState>({
+//   template: templateReducer,
+// }));
+
+// // -----------------------------------------------------------------------
+// // Validators
+// // -----------------------------------------------------------------------
+
+// export const isValidBsUiModelState = (state: any): boolean => {
+//   return !isNil(state)
+//     && state.hasOwnProperty('template') && isValidTemplateState(state.template);
+// };
+
+// export const isValidBsUiModelStateShallow = (state: any): boolean => {
+//   return !isNil(state)
+//     && state.hasOwnProperty('template');
+// };
+
 /** @module Model:base */
 
 import {
@@ -5,16 +68,14 @@ import {
   combineReducers
 } from 'redux';
 import { isNil } from 'lodash';
-import { BsUiModelState } from '../type';
+import { BsPpModelState } from '../type';
 import {
-  BSUIMODEL_BATCH,
-  BsUiModelBaseAction,
-  BsUiModelBatchAction,
+  BsPpModelBaseAction,
 } from './baseAction';
-import {
-  templateReducer,
-  isValidTemplateState,
-} from './template';
+import { hsmReducer } from './hsm';
+import { presentationDataReducer } from './presentation';
+import { playbackReducer } from './playback';
+import { dataFeedReducer, isValidDataFeedState } from './dataFeed';
 
 // -----------------------------------------------------------------------
 // Defaults
@@ -26,37 +87,52 @@ import {
 // Reducers
 // -----------------------------------------------------------------------
 
-export type BsUiReducer = Reducer<BsUiModelState>;
-const enableBatching = (
-    reduce: (state: BsUiModelState, action: BsUiModelBaseAction | BsUiModelBatchAction) => BsUiModelState,
+/** @internal */
+/** @private */
+export type BsUiReducer = Reducer<BsPpModelState>;
+
+/** @internal */
+/** @private */
+export const enableBatching = (
+  reduce: (state: BsPpModelState, action: BsPpModelBaseAction ) => BsPpModelState,
 ): BsUiReducer => {
   return function batchingReducer(
-    state: BsUiModelState,
-    action: BsUiModelBaseAction | BsUiModelBatchAction,
-  ): BsUiModelState {
+    // TEDTODO
+    // state: BsPpModelState,
+    // action: BsPpModelBaseAction,
+    state: any,
+    action: any,
+  ): BsPpModelState {
     switch (action.type) {
-      case BSUIMODEL_BATCH:
-        return (action as BsUiModelBatchAction).payload.reduce(batchingReducer, state);
       default:
         return reduce(state, action);
     }
   };
 };
 
-export const bsUiModelReducer: BsUiReducer = enableBatching(combineReducers<BsUiModelState>({
-  template: templateReducer,
+export const bsPpReducer = enableBatching(combineReducers<BsPpModelState>({
+  hsmState: hsmReducer,
+  presentationData: presentationDataReducer,
+  playback: playbackReducer,
+  arDataFeeds: dataFeedReducer,
 }));
 
 // -----------------------------------------------------------------------
 // Validators
 // -----------------------------------------------------------------------
 
-export const isValidBsUiModelState = (state: any): boolean => {
+/** @internal */
+/** @private */
+// TEDTODO - requires further development
+export function isValidBsPpModelState(state: any): boolean {
   return !isNil(state)
-    && state.hasOwnProperty('template') && isValidTemplateState(state.template);
-};
+  && state.hasOwnProperty('arDataFeeds') && isValidDataFeedState(state.arDataFeeds);
 
-export const isValidBsUiModelStateShallow = (state: any): boolean => {
-  return !isNil(state)
-    && state.hasOwnProperty('template');
-};
+}
+
+/** @internal */
+/** @private */
+// TEDTODO - requires further development
+export function isValidBsPpModelStateShallow(state: any): boolean {
+  return !isNil(state);
+}
